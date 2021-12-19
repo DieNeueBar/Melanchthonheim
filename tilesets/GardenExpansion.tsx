f8218141b636d2767b4b512b9ba150dce0d395f6
DieNeueBar <julia_krueger1@gmx.de>
2021-12-19 01:27:54 +0100

    Map_Einbindung

---
+++ tilesets/GardenExpansion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="GardenExpansion" tilewidth="32" tileheight="32" tilecount="900" columns="18">
+ <image source="../../MelanchthonTest/tilesets/cija_32x32_expansion for Pipoya_shortened_CC0.png" width="576" height="1600"/>
+</tileset>
